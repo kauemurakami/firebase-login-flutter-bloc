c4eaa04d5f4e402cb203e4d31b0d21f520b4624b
Kauê Murakami <kauetmurakami@gmail.com>
2020-05-12 16:51:24 -0300

    user_repository

--- lib/data/Repository/user_repository.dart
+++ lib/data/Repository/user_repository.dart
@@ -8,3 +8,54 @@ class UserRepository {
   UserRepository({FirebaseAuth firebaseAuth, GoogleSignIn googleSignin})
       : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
         _googleSignIn = googleSignin ?? GoogleSignIn();
+
+  //autenticará o usuário usando o GoogleSignInpacote
+  Future<FirebaseUser> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    await _firebaseAuth.signInWithCredential(credential);
+    return _firebaseAuth.currentUser();
+  }
+
+  //método que permitirá aos usuários fazer login com suas próprias credenciais usando FirebaseAuth.
+  Future<void> signInWithCredentials(String email, String password) {
+    return _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  //método que permita aos usuários criar uma conta se optarem por não usar o Login do Google.
+  Future<void> signUp({String email, String password}) async {
+  return await _firebaseAuth.createUserWithEmailAndPassword(
+    email: email,
+    password: password,
+  );
+}
+
+//método para que possamos oferecer aos usuários a opção de efetuar logout e limpar suas informações de perfil do dispositivo.
+Future<void> signOut() async {
+  return Future.wait([
+    _firebaseAuth.signOut(),
+    _googleSignIn.signOut(),
+  ]);
+}
+
+//nos permitir verificar se um usuário já está autenticado e recuperar suas informações.
+Future<bool> isSignedIn() async {
+  final currentUser = await _firebaseAuth.currentUser();
+  return currentUser != null;
+}
+//retornando apenas o endereço de e-mail do usuário atual
+Future<String> getUser() async {
+  return (await _firebaseAuth.currentUser()).email;
+}
+
+
+
+}
